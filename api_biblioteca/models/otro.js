f267b9bde53ea2b315ba98999a331a33eade7cb3
yix109 <100293698+yix109@users.noreply.github.com>
2024-01-31 14:20:54 -0300

    Agregar node_modules

---
+++ api_biblioteca/models/otro.js
@@ -0,0 +1,12 @@
+//Corresponde al archivo Libros.js de la consigna original
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/biblioteca", {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+});
+const LibroSchema = new mongoose.Schema({
+titulo: String,
+autor: String
+}, { collection: 'libros' });
+const Libro = mongoose.model('Libro', LibroSchema);
+module.exports = Libro;
